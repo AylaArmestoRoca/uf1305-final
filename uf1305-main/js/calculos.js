729f2d1c30a1abcb4d9173e0b61863ba7718aa7b
AylaArmestoRoca <aylaarmesto@gmail.com>
2022-03-17 11:46:26 +0100

    +

--- uf1305-main/js/calculos.js
+++ uf1305-main/js/calculos.js
@@ -1,3 +1,6 @@
+// Definir variables
+var n = 0;
+
 // Funcion para leer numeros Reales
 function leerNumero() {
     num = parseInt(
@@ -6,16 +9,38 @@ function leerNumero() {
     return num;
 }
 
-function mostrarResultados() {
-    document.getElementById('td1').innerHTML = n;
-    document.getElementById('td2').innerHTML = factorial();
+function mostrarResultados(nf) {
+    document.getElementById('td21').innerHTML = n;
+    document.getElementById('td22').innerHTML = factorial(nf);
 }
 
-function factorial() {
+function factorial(nf) {
     var factorial = 1;
-    for ( i=1; i <= n; i++ ) {
+    for ( i=1; i <= nf; i++ ) {
         factorial = factorial * i;
     }
 
     return factorial;
 }
+function mostrarFactorial(nf) {
+    document.getElementById('td21').innerHTML = nf;
+    document.getElementById('td22').innerHTML = factorial(nf);
+}
+
+function multiplosDeHasta (n,m){
+        mensaje = "los multiplos de "
+            + n + " hasta " 
+            + m + " son :";
+        multiplo = 0;
+        for(i=1; multiplo<m; i++){
+            multiplo = n*i;
+            mensaje = mensaje + multiplos;
+        }
+        return mensaje;
+    }
+function mostrarMultiplos (n, m){
+    document.getElementById ("td11").innerHTML=n;
+    document.getElementById ("td12").innerHTML=m;
+    document.getElementById ("td13").innerHTML= multiplosDeHasta (n, m);
+}
+
